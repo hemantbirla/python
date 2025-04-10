76f5d0148acc09f7ad1a201d68017aa2a3f8e761
hemantbirla <56964512+hemantbirla@users.noreply.github.com>
2025-04-11 01:45:29 +0530

    Add files via upload

---
+++ mathematical_operations.py
@@ -0,0 +1,24 @@
+#input from user
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+
+first = int(first)
+second = int(second)
+
+#Addition
+add = first + second
+
+#Subtraction
+sub = first - second
+
+#multiplication
+mult = first * second
+
+#Division
+div = first / second
+
+#Output
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mult)
+print("Division: ", div)
---
+++ personalized_greeting.py
@@ -0,0 +1,10 @@
+hello = "Hello, "
+
+#input from user
+first_name = input("Enter your first name: ")
+second_name = input("Enter your second name: ")
+
+greet = "! Welcome to the Pyhon Program."
+
+#output
+print(hello + first_name + " " + second_name + greet)
